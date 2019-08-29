58efa5e6b0675dee633df58b04541906ac8b3e48
webserg@gmail.com <y5OkQ=L8>
2019-08-30 00:10:19 +0300

    filter start

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
-	"github.com/webserg/highloadcup18/readData"
+	d "github.com/webserg/highloadcup18/readData"
 )
 
-var accounts *readData.Accounts
+var accounts *d.Accounts
 
-func init(){
+func init() {
 	fmt.Println("init")
 	var err error
-	accounts, err = readData.ReadData()
+	accounts, err = d.ReadData()
 	check(err)
 	fmt.Println(accounts)
 }
@@ -22,7 +26,7 @@ func init(){
 func main() {
 	// accounts, err := readData.ReadData()
 	http.HandleFunc("/shutdown", shutdown)
-	http.HandleFunc("/accounts/filter", filter)
+	http.HandleFunc("/accounts/filter", filterService)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -30,29 +34,81 @@ func shutdown(res http.ResponseWriter, req *http.Request) {
 	os.Exit(0)
 }
 
-func filter(res http.ResponseWriter, req *http.Request) {
+func filterService(res http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%s \n", req.URL.Path)
 	if req.URL.Path != "/accounts/filter" {
 		http.NotFound(res, req)
+		log.Panic("path not found")
 		return
 	}
-	params := [6]string{"sex", "email", "status", "fname", "sname", "phone"}
+	params := [6]string{"sex_eq", "email_domain", "status", "fname", "sname", "phone"}
 	query := req.URL.Query()
+	log.Println(query)
+	log.Println(len(query))
+	testArray := make([]func(d.Account) bool, 0)
 	for _, k := range params {
 		v, exists := query[k]
 		if exists {
-			fmt.Printf("%s -> %s\n", k, v[0])
+			filterValue := v[0]
 			switch k {
-			case "sex":
-				fmt.Println(v[0])
-				fmt.Println(accounts)
+
+			case "sex_eq":
+				log.Println(k + "=" + filterValue)
+				testArray = append(testArray, func(s d.Account) bool { return s.Sex == filterValue })
+
+			case "email_domain":
+				log.Println(k + "=" + filterValue)
+				testArray = append(testArray, func(s d.Account) bool { return strings.HasSuffix(s.Email, filterValue) })
+
 			}
 		}
 	}
+	S2 := filter2(testArray)
+	fmt.Println(S2)
+	json.NewEncoder(res).Encode(S2)
+}
+
+func filter2(testArray []func(d.Account) bool) (ret []d.Account) {
+	if len(testArray) > 0 {
+		log.Println(testArray)
+		for _, s := range accounts.Accounts {
+			log.Println(s)
+			res := true
+			for _, test := range testArray {
+				log.Println(s)
+				if !test(s) {
+					res = false
+					break
+				}
+			}
+			if res {
+				ret = append(ret, s)
+			}
+		}
+	}
+	return
 }
 
 func check(e error) {
 	if e != nil {
+		log.Panic("error")
 		panic(e)
 	}
+}
+
+func checkHttp(res http.ResponseWriter, req *http.Request, e error) {
+	if e != nil {
+		log.Panic("error")
+		http.Error(res, "error", -1)
+	}
+}
+
+func splitField(f string) (field string, predicate string, e error) {
+	fieldPred := strings.Split(f, "_")
+	if len(fieldPred) != 2 {
+		return "", "", errors.New("field is wrong " + f)
+	}
+	field = fieldPred[0]
+	predicate = fieldPred[1]
+	return field, predicate, nil
 }
